656591b51fc8fbfea1db839462f7c528ef61f285
황주영 <165615814+H-JUYEONG@users.noreply.github.com>
2024-07-12 12:37:04 +0900

    Update

--- src/com/javaex/ex01/RecListApp.java
+++ src/com/javaex/ex01/RecListApp.java
@@ -34,6 +34,8 @@ public class RecListApp {
 
 		System.out.println("#4번째 사각형(4, 5)-->(9, 5)로 width 값 변경======================");
 		// 사각형(2,2) 수정 코드 작성
+		// Rectangle rect = rList.get(2);
+		// rect.setWidth(9);
 		r04.setWidth(9);
 
 		// 리스트 모두를 draw하는 메소드 실행
@@ -55,8 +57,8 @@ public class RecListApp {
 
 		// 내용작성
 		for (int i = 0; i < list.size(); i++) {
-//			Rectangle rect = list.get(i);
-//			rect.draw();
+			// Rectangle rect = list.get(i);
+			// rect.draw();
 			list.get(i).draw();
 		}
 
